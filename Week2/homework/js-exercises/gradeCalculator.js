b52d4aa80446aac831a3dc6d9f501a120a97ce06
mervearas <mervegurzel@icloud.com>
2020-07-26 22:37:07 +0200

    changed condition of grade calculator.

--- Week2/homework/js-exercises/gradeCalculator.js
+++ Week2/homework/js-exercises/gradeCalculator.js
@@ -1,21 +1,21 @@
 'use strict'
 
 function gradeCalculator(score){
-    const scorePercentage = Math.round((score / 100) * 100); //This calculates the percentage of the score.
+    const scorePercentage = (score / 100) * 100; //This calculates the percentage of the score.
     let grade = '';
 
     //Convert percentage into grade!
     if(scorePercentage>=90 && scorePercentage <=100){
         grade = 'A';
-    }else if(scorePercentage>=80 && scorePercentage <= 89){
+    }else if(scorePercentage>=80 && scorePercentage < 90){
         grade = 'B';
-    }else if(scorePercentage>=70 && scorePercentage <= 79){
+    }else if(scorePercentage>=70 && scorePercentage < 80){
         grade = 'C';
-    }else if(scorePercentage>=60 && scorePercentage <= 69){
+    }else if(scorePercentage>=60 && scorePercentage < 70){
         grade = 'D';
-    }else if(scorePercentage>=50 && scorePercentage <= 59){
+    }else if(scorePercentage>=50 && scorePercentage < 60){
         grade = 'E';
-    }else if(scorePercentage>=0 && scorePercentage <= 49){
+    }else if(scorePercentage>=0 && scorePercentage < 50){
         grade = 'F';
     }
 
@@ -24,7 +24,7 @@ function gradeCalculator(score){
 
 //Check each grade!
 gradeCalculator(93)
-gradeCalculator(87)
+gradeCalculator(89.9)
 gradeCalculator(75)
 gradeCalculator(69)
 gradeCalculator(52)
