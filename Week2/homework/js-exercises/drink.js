2ff96f812c592314d0c79adf90bd7c20af602eb0
mervearas <mervegurzel@icloud.com>
2020-07-26 23:52:30 +0200

    used count object to determine to the counts of the drinks

--- Week2/homework/js-exercises/drink.js
+++ Week2/homework/js-exercises/drink.js
@@ -1,10 +1,26 @@
 'use strict';
 let drinkTray = [];
+// create count object to write the count of the drinks
+const count = {};
 const drinkTypes = ["cola", "lemonade", "water"];
+
 for (let i = 0; i < 5; i++) {
-    drinkTray.push(drinkTypes[i]);
-    if (i<= 1) { 
-        drinkTray.push(drinkTypes[i]);
+    // get drink type from drinkTypes array
+    const drinkType = drinkTypes[i];
+    // we need to push each item at least once
+    drinkTray.push(drinkType);
+
+    // cola and lemonade should be written twice in the drinkTray, Check if it is not water.
+    if(drinkType !== "water") {
+        // check if the key is already exist in the count object and count is smaller then 2, then add 1 to the count, and push one more to the drinkTray
+        // else if the key is not found add the key with the value of 1 and push the drink to the drinkTray
+        if(count[drinkType] && count[drinkType] < 2) {
+            count[drinkType] = count[drinkType] + 1;
+            drinkTray.push(drinkType);
+        } else {
+            count[drinkType] = 1;
+            drinkTray.push(drinkType);
+        }
     }
 }
 console.log('"Hey guys, I brought a ' + drinkTray +'!"');
